fac3c16b864be2bb7108591bc7c61071d4b9c0b9
jdtorregrosas <jdtorregrosas@gmail.com>
2016-12-27 11:44:17 -0500

    Add logout function

--- server/app.js
+++ server/app.js
@@ -31,7 +31,22 @@ io.on('connection', function (socket) {
     io.emit('response', players)
   })
   socket.on('sendMessage', function (messageObject) {
-    io.emit('messageResponse', messageObject)
+    var message = messageObject
+    for (let i in players) {
+      if (players[i].username === messageObject.username) {
+        message.color = players[i].color
+      }
+    }
+    console.log(message)
+    io.emit('messageResponse', message)
+  })
+  socket.on('logout', function (username) {
+    for (let i in players) {
+      if (players[i].username === username) {
+        players.splice(i, 1)
+      }
+    }
+    io.emit('response', players)
   })
 })
 
